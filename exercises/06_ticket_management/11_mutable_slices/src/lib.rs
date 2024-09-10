251e889cfdec3c33ebaacf6c87dc56299f640afa
kavehtehrani <kaveh_tehrani@yahoo.com>
2024-09-10 17:40:06 +0300

    progress

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -2,6 +2,20 @@
 //  modifying the input in place.
 //  Does it need to take a `&mut String`? Does a `&mut [str]` work? Why or why not?
 
+// this took quite a while to do. the posted solution is to a different problem at
+// https://github.com/mainmatter/100-exercises-to-learn-rust/blob/solutions/exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+// &mut str works because you can change the content of the string in place, which is effectively a vector of chars.
+// I used unsafe since that's what the make_ascii_lowercase() uses.
+pub fn lowercase(s: &mut str) {
+    for i in 0..s.len() {
+        let bytes = unsafe { s.as_bytes_mut() };
+        for byte in bytes {
+            *byte = byte.to_ascii_lowercase();
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
