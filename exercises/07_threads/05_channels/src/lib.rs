bd3f99121f32552dcb542ffb6bad89a7e2bec578
kavehtehrani <kaveh_tehrani@yahoo.com>
2024-09-11 19:10:31 +0300

    progress

--- exercises/07_threads/05_channels/src/lib.rs
+++ exercises/07_threads/05_channels/src/lib.rs
@@ -1,10 +1,11 @@
 use std::sync::mpsc::{Receiver, Sender};
+use crate::data::TicketDraft;
 
 pub mod data;
 pub mod store;
 
 pub enum Command {
-    Insert(todo!()),
+    Insert(TicketDraft),
 }
 
 // Start the system by spawning the server the thread.
@@ -20,4 +21,16 @@ pub fn launch() -> Sender<Command> {
 //  Enter a loop: wait for a command to show up in
 //  the channel, then execute it, then start waiting
 //  for the next command.
-pub fn server(receiver: Receiver<Command>) {}
+pub fn server(receiver: Receiver<Command>) {
+    let mut ticket_store = store::TicketStore::new();
+    loop {
+        match receiver.recv() {
+            Ok(Command::Insert(ticket)) => {
+                ticket_store.add_ticket(ticket);
+            }
+            Err(_) => {
+                break;
+            }
+        }
+    }
+}
